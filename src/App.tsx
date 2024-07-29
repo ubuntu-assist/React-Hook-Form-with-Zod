3111f7fee33d56e3ac9fb7723340dfd164b69e0a
weshare237 <fopaduclair2000@gmail.com>
2024-07-29 15:30:45 +0100

    Template for react form validation

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SimpleForm from './components/1-simple-form'
+
+function App() {
+  return (
+    <div>
+      <h1>React Hook Form Tutorial</h1>
+      <SimpleForm />
+    </div>
+  )
+}
+
+export default App
